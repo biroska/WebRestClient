7ffa9a73b36c2e7915e9a5e9633d12c15ea0d1a1
Aimberê Galdino <biroska@gmail.com>
2015-09-05 11:46:40 -0300

    Feita chamada para o web service, falta tratar o retorno

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -3,5 +3,18 @@ package util;
 public interface Constants {
 	
 	public static final String PAGES_FOLDER = "pages/";
+	
+	public static final String WEBSERVICE_PREFIX = "http://localhost:8080/WebRest/";
 
+	public interface WEBSERVICE_ADDRESS {
+		public static final String GET_ALL = WEBSERVICE_PREFIX + "bandas";
+	}
+
+	public interface REQUEST_TYPE {
+		public static final String GET = "GET";
+	}
+	
+	public interface REQUEST_PROPERTY {
+		public static final String XML = "text/xml";
+	}
 }
--- src/webService/WebServiceAccess.java
+++ src/webService/WebServiceAccess.java
@@ -1,8 +1,15 @@
 package webService;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import util.Constants;
 import entidade.Album;
 import entidade.Banda;
 
@@ -30,6 +37,38 @@ public class WebServiceAccess implements ServiceAccess {
 	
 	public List<Banda> getAllBandas() {
 
+		try {
+			 
+			URL url = new URL( Constants.WEBSERVICE_ADDRESS.GET_ALL );
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod( Constants.REQUEST_TYPE.GET );
+			conn.setRequestProperty("Accept", Constants.REQUEST_PROPERTY.XML );
+	 
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+	 
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			conn.disconnect();
+	 
+		  } catch (MalformedURLException e) {
+	 
+			e.printStackTrace();
+	 
+		  } catch (IOException e) {
+	 
+			e.printStackTrace();
+		  }
+		
+		
 		return listaBandas;
 	}
 	
